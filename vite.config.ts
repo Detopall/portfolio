ca0bcdab15ace99bc05aa71d8cc4d7d06fc10d60
denis.topallaj <denis.topallaj@student.howest.be>
2023-05-28 16:02:18 +0200

    changed: vite.config.ts so that images are deployed on firebase. Needed to move images in public folder and change paths of images, works now on firebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-      output: {
-        assetFileNames: 'assets/[name].[ext]',
-      },
-    },
-  },
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: ({ name }) => {
+					if (/\.(gif|jpeg|png|svg)$/.test(name ?? "")) {
+						return "assets/images/[name][extname]";
+					}
+
+					if (/\.css$/.test(name ?? "")) {
+						return "assets/css/[name][extname]";
+					}
+
+					// default value
+					// ref: https://rollupjs.org/guide/en/#outputassetfilenames
+					return "assets/[name][extname]";
+				},
+			},
+		},
+	},
 });
