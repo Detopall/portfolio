3d78b5d2c11be2184b7f6559c09b13c30d1f7451
Detopall <denis.topallaj13@gmail.com>
2024-09-26 10:04:17 +0200

    modified the about cv button

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -42,7 +42,7 @@ function About() {
 
 				{pdfLink ? (
 					<a href={pdfLink} target="_blank">
-						<img src="https://img.shields.io/badge/CV-red?style=for-the-badge&logo=microsoftword&logoColor=white" />
+						<img src="https://img.shields.io/badge/CV-white?logo=google-docs&logoColor=red" />
 					</a>
 				) : (
 					<div>PDF Loading ... </div>
